5b658f5c2c8733eada4b176ed9b3492370062a17
Andrés <na2003ml.am@gmail.com>
2023-06-20 20:43:43 -0500

    Prueba de la función ValidarPass

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 var cuentas = [
-    { nombre: "Mali", saldo: 200, pass: "Malinali2211"}
-    { nombre: "Gera", saldo: 290, pass: "gerardO034"}
+    { nombre: "Mali", saldo: 200, pass: "Malinali2211"},
+    { nombre: "Gera", saldo: 290, pass: "gerardO034"},
     { nombre: "Maui", saldo: 67, pass: "Mau1-2"}
   ];
+
+//Prueba de la función ValidarPass
+
+let pass=prompt("Favor ingrese la contraseña")
+
+let access=ValidarPass(pass,cuentas[0].pass)
+
+if (access) {
+    alert("Contraseña correcta")
+} else {
+    alert("Contraseña errada")
+}
+//Fin de prueba
 
 function ValidarPass(UserPass, SavePass) {
     if (UserPass===SavePass) {
